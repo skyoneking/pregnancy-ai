00990f5856c3e7392b154e900bc8837312da93bd
蒋从亮 <111@111.com>
2026-04-10 16:35:52 +0800

    feat: add ai-elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import "./globals.css";
-import { ToastProvider } from "@/app/contexts/ToastContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToastProvider>
-          {children}
-        </ToastProvider>
+        <TooltipProvider>{children}</TooltipProvider>
       </body>
     </html>
   );
